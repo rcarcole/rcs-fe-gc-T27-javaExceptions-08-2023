7cc1f7f4ba47b30fbb6a499ed86a0f32be95dc66
rcarcole <55997367+rcarcole@users.noreply.github.com>
2023-08-31 18:43:49 +0200

    Update Ex03.java

--- Ejercicio03/Ex03.java
+++ Ejercicio03/Ex03.java
@@ -2,29 +2,23 @@ package Ejercicio03;
 
 import java.util.Random;
 
-<<<<<<< HEAD
 //Definición de la excepción
-=======
-//Definición de la excepción personalizada
->>>>>>> a71aec1003addbe29850adfe028f0ac9e33e3627
 class NumeroImparException extends Exception {
  public NumeroImparException(String message) {
      super(message);
  }
 }
 
-<<<<<<< HEAD
+
 class NumeroAleatorioParImpar {
  public void generarYVerificar() throws NumeroImparException {
      Random random = new Random();
      int numeroAleatorio = random.nextInt(1000);
-=======
-//Clase que genera un número aleatorio y verifica si es par o impar
+
 class NumeroAleatorioParImpar {
  public void generarYVerificar() throws NumeroImparException {
      Random random = new Random();
-     int numeroAleatorio = random.nextInt(1000); // Genera un número aleatorio entre 0 y 999
->>>>>>> a71aec1003addbe29850adfe028f0ac9e33e3627
+     int numeroAleatorio = random.nextInt(1000); 
      
      System.out.println("Generando número aleatorio...");
      System.out.println("El número aleatorio generado es: " + numeroAleatorio);
@@ -37,10 +31,6 @@ class NumeroAleatorioParImpar {
  }
 }
 
-<<<<<<< HEAD
-=======
-//Clase principal que ejecuta el programa
->>>>>>> a71aec1003addbe29850adfe028f0ac9e33e3627
 public class Ex03 {
  public static void main(String[] args) {
      NumeroAleatorioParImpar generador = new NumeroAleatorioParImpar();
